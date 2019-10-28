f1f07dec1ad5bce2fd37398385de30ac4a3a1a23
Chen <541628730@qq.com>
2019-10-28 11:01:15 +0800

    测试hello world
    测试hello world

--- appupdater/app/src/main/java/com/chen/appupdater/MainActivity.java
+++ appupdater/app/src/main/java/com/chen/appupdater/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this,"hello world",Toast.LENGTH_SHORT).show();
         mBtnUpdater=(Button)findViewById(R.id.id_btn_updater);
         mBtnUpdater.setOnClickListener(new View.OnClickListener() {
             @Override
